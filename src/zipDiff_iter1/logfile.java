f0dfe7f8d3d5a89565374f94584bfce43fb3043d
rawanManasra <rwan.mnasra@gmail.com>
2018-02-27 15:18:16 +0200

    fv

--- src/zipDiff_iter1/logfile.java
+++ src/zipDiff_iter1/logfile.java
@@ -1,13 +1,14 @@
 package zipDiff_iter1;
 
 import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
+
+import org.apache.log4j.Logger;
 
 public class logfile {
-	private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
 	public final String username = System.getProperty("user.name");
 	public Timestamp logTime;
 	OperationType opType;
+	final static Logger logger = Logger.getLogger(logfile.class);
 
 	public static void main(String[] args) {
 		String username = System.getProperty("user.name");
