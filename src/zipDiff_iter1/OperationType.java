f4a052430e227f3dfb0264d235a44482dfc2fbf3
rawanManasra <rwan.mnasra@gmail.com>
2018-02-27 22:10:28 +0200

    jjjj

--- src/zipDiff_iter1/OperationType.java
+++ src/zipDiff_iter1/OperationType.java
@@ -1,6 +1,11 @@
 package zipDiff_iter1;
 
-public class OperationType {
-	String OpType;
+public interface OperationType {
+	public final String TURN_ON = "turn on";
+	public final String TURN_OFF = "turn off";
+	public final String ZIP_CONTENT_COMPARISON = "zip content comparison";
+	public final String DIFF_CREATION = "ExampleDB";
+	public final String PATCH_CREATION = "patch creation";
+	public final String PATCH_APPLICATION = "patch application";
 
 }
--- src/zipDiff_iter1/logfile.java
+++ src/zipDiff_iter1/logfile.java
@@ -1,18 +1,31 @@
 package zipDiff_iter1;
 
-import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
 public class logfile {
 	public final String username = System.getProperty("user.name");
-	public Timestamp logTime;
-	OperationType opType;
+	public String opType;
 	final static Logger logger = Logger.getLogger(logfile.class);
+	public final String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
 
-	public static void main(String[] args) {
-		String username = System.getProperty("user.name");
-		System.out.println("username = " + username);
+	public logfile(String string) {
+		opType = string;
 	}
 
+	public String toString() {
+		return "time : " + timeStamp + ", user: " + username + " ,Operation: " + opType;
+	}
+
+	public static void main(String[] args) {
+		logfile lf = new logfile(OperationType.TURN_ON);
+		BasicConfigurator.configure();
+
+		if (logger.isInfoEnabled()) {
+			logger.info(lf);
+		}
+	}
 }
