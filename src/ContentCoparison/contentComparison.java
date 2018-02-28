f9d0e9401ff87461bf637bffb41b2e68f69b5e15
rawanManasra <rwan.mnasra@gmail.com>
2018-02-28 11:10:13 +0200

    kldkd

---
+++ src/ContentCoparison/contentComparison.java
@@ -0,0 +1,72 @@
+package ContentCoparison;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class contentComparison {
+
+	static MessageDigest sha256 = null;
+	public FileInputStream file1 = null;
+	public FileInputStream file2 = null;
+
+	public contentComparison(FileInputStream in1, FileInputStream in2) {
+		file1 = in1;
+		file2 = in2;
+	}
+
+	public boolean compare2files() {
+		return hashFileStream(file1) == hashFileStream(file2);
+	}
+
+	/**
+	 * Computes a SHA2-256 digest on the file contents.
+	 * 
+	 * @param stream
+	 *            The input stream to process
+	 * @return The SHA2-256 digest of the file contents encoded in Base64 encoding
+	 *         (not URL safe). Returns null if the hash digest fails.
+	 */
+	public String hashFileStream(InputStream stream) {
+
+		if (sha256 == null) {
+			try {
+				sha256 = MessageDigest.getInstance("SHA-256");
+			} catch (NoSuchAlgorithmException e) {
+				// this should never happen
+				e.printStackTrace();
+				return null;
+			}
+		}
+
+		try {
+			// reset the digest state machine
+			sha256.reset();
+
+			// open the stream and hash the contents
+			byte[] buf = new byte[4096];
+			int bytesRead = 0;
+			while ((bytesRead = stream.read(buf)) > 0) {
+				sha256.update(buf, 0, bytesRead);
+			}
+			byte[] digest = sha256.digest();
+			return Base64.getEncoder().encodeToString(digest);
+		} catch (IOException iox) {
+			System.out.println("Error computing hash on the input stream");
+		}
+		return null;
+	}
+
+	public static void main(String args[]) throws IOException, FileNotFoundException {
+
+		FileInputStream in1 = new FileInputStream("D:/file1.txt");
+		FileInputStream in2 = new FileInputStream("D:/file2.txt");
+		contentComparison mm = new contentComparison(in1, in2);
+		System.out.println(mm.compare2files());
+	}
+
+}
--- src/zipDiff_iter1.java
+++ src/zipDiff_iter1.java
@@ -1,41 +1,58 @@
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-
-public class zipDiff_iter1 {
-
-	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					zipDiff_iter1 window = new zipDiff_iter1();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the application.
-	 */
-	public zipDiff_iter1() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-
-}
+// import java.awt.Color;
+// import java.awt.EventQueue;
+// import java.awt.Font;
+//
+// import javax.swing.JButton;
+// import javax.swing.JFrame;
+//
+// public class zipDiff_iter1 {
+//
+// private JFrame frame;
+//
+// /**
+// * Launch the application.
+// */
+// public static void main(String[] args) {
+// EventQueue.invokeLater(new Runnable() {
+// public void run() {
+// try {
+// zipDiff_iter1 window = new zipDiff_iter1();
+// window.frame.setVisible(true);
+// } catch (Exception e) {
+// e.printStackTrace();
+// }
+// }
+// });
+// }
+//
+// /**
+// * Create the application.
+// */
+// public zipDiff_iter1() {
+// initialize();
+// }
+//
+// /**
+// * Initialize the contents of the frame.
+// */
+// private void initialize() {
+// frame = new JFrame();
+// frame.getContentPane().setForeground(new Color(0, 255, 51));
+// frame.setBounds(100, 100, 888, 614);
+// frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+// frame.getContentPane().setLayout(null);
+//
+// JButton btnNewButton = new JButton("zip conent comparison");
+// btnNewButton.setForeground(new Color(0, 204, 51));
+// btnNewButton.setBackground(new Color(51, 153, 102));
+// btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 18));
+// // btnNewButton.addActionListener(new ActionListener() {
+//
+// // public void actionPerformed(ActionEvent e) {
+// // JOptionPane.showMessageDialog(null, "hii");
+// // }
+// // });
+// btnNewButton.setBounds(26, 32, 272, 45);
+// frame.getContentPane().add(btnNewButton);
+// }
+// }
