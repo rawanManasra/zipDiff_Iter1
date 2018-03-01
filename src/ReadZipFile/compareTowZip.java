5b0a8f036fd6dc1b04680c7ddf95990fcf18539b
rawanManasra <rwan.mnasra@gmail.com>
2018-03-01 09:25:31 +0200

    jdjs

--- src/ReadZipFile/compareTowZip.java
+++ src/ReadZipFile/compareTowZip.java
@@ -31,6 +31,42 @@ public class compareTowZip {
 		return arr;
 	}
 
+	public void findIdenticalFiles() throws IOException {
+		int count1 = 0;
+		int count2 = 0;
+		metadataAndPathComparison metadataAndPath = new metadataAndPathComparison();
+		ArrayList<ZipEntry> arr1 = new ArrayList<ZipEntry>(copyToArrayList(zip1));
+		ArrayList<ZipEntry> arr2 = new ArrayList<ZipEntry>(copyToArrayList(zip2));
+		for (Iterator<ZipEntry> it1 = arr1.iterator(); it1.hasNext();) {
+			count2 = count1;
+			ZipEntry entry1 = it1.next();
+			InputStream stream1 = zip1.getInputStream(entry1);
+			for (Iterator<ZipEntry> it2 = arr2.iterator(); it2.hasNext();) {
+				ZipEntry entry2 = it2.next();
+				InputStream stream2 = zip2.getInputStream(entry2);
+				inputStreamContentComparison compare2streams = new inputStreamContentComparison(stream1, stream2);
+				/* check if the content of 2 entries are the same */
+				if (compare2streams.compare2files()) {
+					/* check if the 2 entries has the same name */
+					if (metadataAndPath.metadataTest(entry1, entry2)) {
+						// TODO get path for each entry and what does it mean by path, because each
+						// entry is in a different archive
+						if (metadataAndPath.pathTest(entry1, entry2, zip1, zip2)) {
+							System.out.println("Identical " + entry1.getName() + " " + entry2.getName());
+							it1.remove();
+							it2.remove();
+							count1++;
+							break;
+						}
+					}
+				}
+			}
+			if (count1 > count2) {
+				continue;
+			}
+		}
+	}
+
 	public void cmp() throws IOException {
 		int count1 = 0;
 		int count2 = 0;
@@ -87,11 +123,11 @@ public class compareTowZip {
 		ZipFile zip1 = new ZipFile("C:\\Users\\rawan\\Desktop\\file2copy.zip");
 		ZipFile zip2 = new ZipFile("C:\\Users\\rawan\\Desktop\\file2.zip");
 		compareTowZip cmp2zip = new compareTowZip(zip1, zip2);
-		cmp2zip.cmp();
+		cmp2zip.findIdenticalFiles();
 		// Path p = Paths.get(zip1.getName()).toAbsolutePath().getRoot();
 		// final Enumeration<? extends ZipEntry> entries = zip1.entries();
 		// while (entries.hasMoreElements()) {
-		// final ZipEntry entry = entries.nextElement();
+		// final ZipEntry entry = entries.nextElement();w
 		// Path p2 = Paths.get(entry.getName()).toAbsolutePath().relativize(p);
 		// System.out.println(p2.toString());
 		// Path p3 = Paths.get(entry.getName());
